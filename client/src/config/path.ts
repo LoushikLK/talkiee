9915c74a3b238cfd5fdb4f0320fcaee8a7b24557
Loushik <loushikkumargiri@gmail.com>
2022-06-15 21:29:32 +0530

    making secure socket

--- client/src/config/path.ts
+++ client/src/config/path.ts
@@ -6,11 +6,10 @@ export const userSelf = "https://talkieeapp.herokuapp.com/self";
 
 export const loginPath = "https://talkieeapp.herokuapp.com/login";
 
-export const friendsPath = "https://talkieeapp.herokuapp.com/friends";
+export const friendsPath = "/friends";
 
-export const getMessagePath = "https://talkieeapp.herokuapp.com/private";
+export const getMessagePath = "/message/private";
 
-export const sendMessagePath =
-  "https://talkieeapp.herokuapp.com/message/private";
+export const sendMessagePath = "/message/private";
 
-export const socketPath = "ws://talkieeapp.herokuapp.com";
+export const socketPath = "wss://talkieeapp.herokuapp.com";
